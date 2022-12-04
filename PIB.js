1364fbd4b57e524bd22b1f24d3f19960007dbe25
nisrineelam <117943816+nisrineelam@users.noreply.github.com>
2022-12-04 15:13:14 +0100

    Add files via upload

---
+++ PIB.js
@@ -0,0 +1,19 @@
+var PIB={
+"type": "FeatureCollection",
+"name": "PIB TEST",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "CODE_REGIO": "03.", "PIB": 89267.0 }, "geometry": { "type": "Point", "coordinates": [ -4.790219340859299, 33.622240737622732 ] } },
+{ "type": "Feature", "properties": { "CODE_REGIO": "02.", "PIB": 59733.0 }, "geometry": { "type": "Point", "coordinates": [ -2.619753930928905, 32.983868558231443 ] } },
+{ "type": "Feature", "properties": { "CODE_REGIO": "01.", "PIB": 125536.0 }, "geometry": { "type": "Point", "coordinates": [ -5.268998475402768, 35.218171186100967 ] } },
+{ "type": "Feature", "properties": { "CODE_REGIO": "04.", "PIB": 183359.0 }, "geometry": { "type": "Point", "coordinates": [ -6.130800917581013, 33.781833782470557 ] } },
+{ "type": "Feature", "properties": { "CODE_REGIO": "05.", "PIB": 61756.0 }, "geometry": { "type": "Point", "coordinates": [ -6.290393962428835, 32.505089423687977 ] } },
+{ "type": "Feature", "properties": { "CODE_REGIO": "07.", "PIB": 90953.0 }, "geometry": { "type": "Point", "coordinates": [ -8.524696590298358, 31.770961417387987 ] } },
+{ "type": "Feature", "properties": { "CODE_REGIO": "06.", "PIB": 371368.0 }, "geometry": { "type": "Point", "coordinates": [ -7.535219712241854, 32.856194122353187 ] } },
+{ "type": "Feature", "properties": { "CODE_REGIO": "08.", "PIB": 36692.0 }, "geometry": { "type": "Point", "coordinates": [ -4.822137949828862, 30.845321757270614 ] } },
+{ "type": "Feature", "properties": { "CODE_REGIO": "09.", "PIB": 75429.0 }, "geometry": { "type": "Point", "coordinates": [ -8.237429109572275, 29.792007661274983 ] } },
+{ "type": "Feature", "properties": { "CODE_REGIO": "10.", "PIB": 17297.0 }, "geometry": { "type": "Point", "coordinates": [ -9.418417641446167, 28.132239994857621 ] } },
+{ "type": "Feature", "properties": { "CODE_REGIO": "11.", "PIB": 25762.0 }, "geometry": { "type": "Point", "coordinates": [ -11.780394705193949, 27.047007289892427 ] } },
+{ "type": "Feature", "properties": { "CODE_REGIO": "12.", "PIB": 13821.0 }, "geometry": { "type": "Point", "coordinates": [ -14.648909583803642, 23.622787607799776 ] } }
+]
+}
